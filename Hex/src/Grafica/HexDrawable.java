61ef54e416e50cb8387ca82e38a73dea01e8f69d
Rhalex <56119337+Rhalex@users.noreply.github.com>
2020-03-12 19:09:38 +0100

    Consegna Progetto

---
+++ Hex/src/Grafica/HexDrawable.java
@@ -0,0 +1,35 @@
+package Grafica;
+
+import java.awt.Color;
+
+public class HexDrawable {
+	int x;
+	int y;
+	Color color;
+	int idLogicalHex;
+	
+	public HexDrawable(int x, int y, Color color, int id) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.idLogicalHex = id;
+	}
+
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if( obj instanceof HexDrawable)
+		{
+			HexDrawable h = (HexDrawable) obj;
+			return (h.x == x) && (h.y == y) && (h.color == color);
+		}	
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "HexDrawable [x=" + x + ", y=" + y + ", color=" + color + "]";
+	}	
+	
+}
